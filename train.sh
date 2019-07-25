ed186f540be8c64f44afb87cfb4819a5204f43be
David Trimoulet <davidt@hotmail.fr>
2019-07-25 15:23:21 +0200

    [UPDATE] changing parameters to use local disk to store tensor

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ludwig experiment --data_csv ../Visual_Genome/region_pairs --model_definition_file model_definition.yaml
+ludwig experiment --data_csv region_pairs --model_definition_file model_definition.yaml
